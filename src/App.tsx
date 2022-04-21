47745740aae0cc7ebce8be44b34b41477911227c
mariamnersisyan97@gmail.com <43486641+mariamnersisyan97@users.noreply.github.com>
2022-04-21 18:58:10 -0400

    input form added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
+// import TodoList from './TodoForm';
+import React, { useState } from 'react';
+
 
 export const App = () => {
+  // const [todos, setTodos] = useState('')
   return(
+    
     <div>
     <header style={{ textAlign: 'center'}}>
      <h1> To Do List</h1>
      </header>
+        <div>
+        <div className="ui input"><input type="text" placeholder="Search..."/></div>
+        </div>
      <button style={{ textAlign: 'center'}}> Add Task </button>
+     
      </div>
   )
 
 };
-// const handleTaskItem = () => {
-
-// }
